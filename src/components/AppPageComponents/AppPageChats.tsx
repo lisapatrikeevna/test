984239f11bee8cfd3c44936fd159d25dd5ecd488
Serg <serega101617@gmail.com>
2024-05-15 16:44:47 +0200

    added AppPage

---
+++ src/components/AppPageComponents/AppPageChats.tsx
@@ -0,0 +1,54 @@
+import { Avatar, Divider, Stack, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { data } from '../../components/ProfileComponents/utils';
+
+const AppPageChats = () => {
+  const [users, setUsers] = useState(data);
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        border: '2px solid black',
+        width: '100%',
+        borderRadius: '5px',
+      }}
+    >
+      <Stack
+        direction="column"
+        spacing={2}
+        padding={1}
+        borderRight="1px solid black"
+      >
+        {users.map((elem) => (
+          <Avatar key={elem.id} src={elem.img} />
+        ))}
+      </Stack>
+      <Divider sx={{ color: 'black' }} />
+      <Stack width="100%" padding={1}>
+        <Stack sx={{ textAlign: 'right', marginBottom: '7px' }}>
+          <Typography>Hi</Typography>
+          <Typography>How are you?</Typography>
+        </Stack>
+        <Stack sx={{ marginBottom: '7px' }}>
+          <Typography>Hi</Typography>
+          <Typography>How are you?</Typography>
+        </Stack>
+        <Stack sx={{ textAlign: 'right', marginBottom: '7px' }}>
+          <Typography>Hi</Typography>
+          <Typography>How are you?</Typography>
+        </Stack>
+        <Stack sx={{ marginBottom: '7px' }}>
+          <Typography>Hi</Typography>
+          <Typography>How are you?</Typography>
+        </Stack>
+        <Stack sx={{ textAlign: 'right', marginBottom: '7px' }}>
+          <Typography>How are you?</Typography>
+        </Stack>
+        <Stack>
+          <TextField size="small" />
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+export default AppPageChats;
