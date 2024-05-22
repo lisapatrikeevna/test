0a794d1adbac02afad20dc8affb1dd8e0012783f
mavr1ke <mainstip@mail.ru>
2024-05-22 14:25:44 +0300

    minor changes

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -102,7 +102,7 @@ const Pricing = () => {
             <AppBar position="static" color="default" elevation={0} sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}>
                 <Toolbar sx={{ flexWrap: 'wrap' }}>
                     <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-                        Company name
+                        NeoX
                     </Typography>
                     <FormControlLabel
                         control={<Switch checked={isYearly} onChange={toggleYearly} />}
@@ -112,7 +112,7 @@ const Pricing = () => {
                 </Toolbar>
             </AppBar>
 
-            <Container disableGutters maxWidth={false} component="main">
+            <Container disableGutters maxWidth={false} component="main" sx={{ pb: 2 }}>
                 <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
                     Pricing
                 </Typography>
