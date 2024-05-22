2960677fcb15e91d618d5984aec3855c36a9609f
Kirrrra <kirrrra007@gmail.com>
2024-05-22 12:52:05 +0200

    fast fix

--- src/App.tsx
+++ src/App.tsx
@@ -30,28 +30,6 @@ import {
   mediaIdPath,
   // aboutUsPath,
   mediaPath,
-<<<<<<< HEAD
-  productsPath,
-  settingsPath,
-  callsPath, sideBarPath, certificatePath,
-  newGroupPath,
-  newChannelPath,
-  contactsPath,
-  moreVideosPath,
-  vrPath
-} from "./configs/RouteConfig.tsx";
-import ChannelPage from "./pages/Videos/ChannelPage.tsx";
-import CertificateGenerator from "./pages/cert/CertificateGenerator.tsx";
-import UserChannelPage from "./pages/Videos/UserChannelPage.tsx";
-import NewGroup from "./pages/NewGroup.tsx";
-import NewChannel from "./pages/NewChannel.tsx";
-import Contacts from "./pages/Contacts.tsx";
-import { Box } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { lightTheme, darkTheme } from './theme.tsx';
-import { ThemeContext } from "./contexts/ThemeContext";
-import MoreVideos from "./pages/Videos/MoreVideos.tsx";
-=======
   // productsPath,
   // settingsPath,
   // callsPath,
@@ -72,7 +50,7 @@ import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { lightTheme, darkTheme } from './theme.tsx';
 import { ThemeContext } from './contexts/ThemeContext';
->>>>>>> develop
+
 
 const App: FC = () => {
   // Initialize theme state with light theme as default
@@ -120,59 +98,7 @@ const App: FC = () => {
   const allowedUsernames = ['AdrianAdrian', 'Adrian Lieblich', 'RomarioFisch'];
 
   return (
-<<<<<<< HEAD
-    <ThemeContext.Provider value={{ theme: theme, setTheme }}>
-      <ThemeProvider theme={muiTheme}>
-        <Router>
-          <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-            {isLoggedIn ? (
-              <Box sx={{
-                display: 'flex',
-                height: '100vh'
-              }}>
-                <SideBar />
-                <Box sx={{
-                  flexGrow: 1,
-                  overflow: 'auto',
-                  width: '100%'
-                }}>
-                  <Routes>
-                    <Route path={homePath} element={<Home />} />
-                    <Route path={chatsPath} element={<Chats />} />
-                    <Route path={aboutUsPath} element={<AboutUs />} />
-                    <Route path={mediaPath} element={<VideosMainPage />} />
-                    <Route path={mediaIdPath} element={<VideoPage />} />
-                    <Route path={callsPath} element={<Calls />} />
-                    <Route path={settingsPath} element={<Settings />} />
-                    <Route path={sideBarPath} element={<SideBar />} />
-                    <Route path={appPagePath} element={<AppPage />} />
-                    <Route path={channelPagePrototypePath} element={<ChannelPage />} />
-                    <Route path={channelEditPrototypePath} element={<UserChannelPage />} />
-                    <Route path={productsPath} element={<ElementsShowroom />} />
-                    <Route path={newGroupPath} element={<NewGroup />} />
-                    <Route path={newChannelPath} element={<NewChannel />} />
-                    <Route path={contactsPath} element={<Contacts />} />
-                    <Route path={moreVideosPath} element={<MoreVideos />} />
-                    <Route path={vrPath} element={<VR />} />
-                    <Route path={certificatePath} element={allowedUsernames.includes(username) ? <CertificateGenerator /> : ""} />
-                  </Routes>
-                </Box>
-              </Box>
-            ) : (
-              <>
-                <HeaderAndMainPage
-                  activeSection={activeSection}
-                  setActiveSection={setActiveSection}
-                />
-              </>
-            )}
 
-            <LoginModal isOpen={isModalOpen} onClose={handleCloseModal} />
-          </AuthContext.Provider>
-        </Router>
-      </ThemeProvider>
-    </ThemeContext.Provider>
-=======
       <ThemeContext.Provider value={{ theme: theme, setTheme }}>
         <ThemeProvider theme={muiTheme}>
           <Router>
@@ -243,7 +169,6 @@ const App: FC = () => {
           </Router>
         </ThemeProvider>
       </ThemeContext.Provider>
->>>>>>> develop
   );
 };
 
