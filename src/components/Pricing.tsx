c51cc3aa12dce57c9980df6456a98a560a0ab045
mavr1ke <mainstip@mail.ru>
2024-05-28 12:18:00 +0300

    pricing changes

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -10,7 +10,14 @@ import { styled } from '@mui/system';
 const CustomContainer = styled(Container)({
     width: '100%',
     margin: '0 auto',
-    padding: 0,
+    padding: 13,
+});
+
+const ScrollableContainer = styled(Box)({
+    width: '100%',
+    overflowY: 'auto',
+    maxHeight: 'calc(100vh - 440px)', 
+    // paddingBottom: '10px'
 });
 
 const tiers = [
@@ -121,59 +128,61 @@ const Pricing = () => {
                 </Typography>
             </CustomContainer>
 
-            <CustomContainer className="Pricing-container" maxWidth="xl">
-                <Grid container spacing={4} alignItems="flex-end" justifyContent={'center'}>
-                    {tiers.map((tier) => (
-                        <Grid item key={tier.title} xs={12} sm={6} md={4} lg={3} xl={2}>
-                            <Card>
-                                <CardHeader
-                                    title={tier.title}
-                                    subheader={tier.subheader}
-                                    titleTypographyProps={{ align: 'center' }}
-                                    action={tier.title === 'Prime' ? <StarIcon /> : null}
-                                    subheaderTypographyProps={{
-                                        align: 'center',
-                                    }}
-                                    sx={{
-                                        backgroundColor: (theme) =>
-                                            theme.palette.mode === 'light'
-                                                ? theme.palette.grey[200]
-                                                : theme.palette.grey[700],
-                                    }}
-                                />
-                                <CardContent>
-                                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'baseline', mb: 2 }}>
-                                        <Typography component="h2" variant={tier.title==='Enterprise'?"h6":"h3"} color="text.primary">
-                                            {tier.price ? `$${calculatePrice(tier.price)}` : tier.title==='Enterprise'?'Contact us':'Free'}
-                                        </Typography>
-                                        <Typography variant= "h6" color="text.secondary">
-                                            {tier.title==='Enterprise'||tier.title==='Free'?'':`${isYearly ? 'yr' : 'mo'}`}
-                                        </Typography>
-                                    </Box>
-                                    <ul>
-                                        {tier.description.map((line) => (
-                                            <Typography component="li" variant="subtitle1" align="center" key={line}>
-                                                {line}
+            <ScrollableContainer>
+                <CustomContainer className="Pricing-container" maxWidth="xl">
+                    <Grid container spacing={4} alignItems="flex-end" justifyContent={'center'}>
+                        {tiers.map((tier) => (
+                            <Grid item key={tier.title} xs={12} sm={6} md={4} lg={3} xl={2}>
+                                <Card>
+                                    <CardHeader
+                                        title={tier.title}
+                                        subheader={tier.subheader}
+                                        titleTypographyProps={{ align: 'center' }}
+                                        action={tier.title === 'Prime' ? <StarIcon /> : null}
+                                        subheaderTypographyProps={{
+                                            align: 'center',
+                                        }}
+                                        sx={{
+                                            backgroundColor: (theme) =>
+                                                theme.palette.mode === 'light'
+                                                    ? theme.palette.grey[200]
+                                                    : theme.palette.grey[700],
+                                        }}
+                                    />
+                                    <CardContent>
+                                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'baseline', mb: 2 }}>
+                                            <Typography component="h2" variant={tier.title === 'Enterprise' ? "h6" : "h3"} color="text.primary">
+                                                {tier.price ? `$${calculatePrice(tier.price)}` : tier.title === 'Enterprise' ? 'Contact us' : 'Free'}
                                             </Typography>
-                                        ))}
-                                        <Typography component="li" variant="subtitle1" align="center">
-                                            <CheckCircleOutlineIcon sx={{ verticalAlign: 'middle' }} /> Feature 1
-                                        </Typography>
-                                        <Typography component="li" variant="subtitle1" align="center">
-                                            <CheckCircleOutlineIcon sx={{ verticalAlign: 'middle' }} /> Feature 2
-                                        </Typography>
-                                    </ul>
-                                </CardContent>
-                                <CardActions>
-                                    <Button fullWidth variant={tier.buttonVariant as 'text' | 'outlined' | 'contained'}>
-                                        {tier.buttonText}
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </CustomContainer>
+                                            <Typography variant="h6" color="text.secondary">
+                                                {tier.title === 'Enterprise' || tier.title === 'Free' ? '' : `${isYearly ? 'yr' : 'mo'}`}
+                                            </Typography>
+                                        </Box>
+                                        <ul>
+                                            {tier.description.map((line) => (
+                                                <Typography component="li" variant="subtitle1" align="center" key={line}>
+                                                    {line}
+                                                </Typography>
+                                            ))}
+                                            <Typography component="li" variant="subtitle1" align="center">
+                                                <CheckCircleOutlineIcon sx={{ verticalAlign: 'middle' }} /> Feature 1
+                                            </Typography>
+                                            <Typography component="li" variant="subtitle1" align="center">
+                                                <CheckCircleOutlineIcon sx={{ verticalAlign: 'middle' }} /> Feature 2
+                                            </Typography>
+                                        </ul>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button fullWidth variant={tier.buttonVariant as 'text' | 'outlined' | 'contained'}>
+                                            {tier.buttonText}
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </CustomContainer>
+            </ScrollableContainer>
         </Box>
     );
 };
