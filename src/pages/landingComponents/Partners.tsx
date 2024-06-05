2fc7d2d06cb175735e8d7c714b099c8b707a2dbc
mavr1ke <mainstip@mail.ru>
2024-06-05 12:59:33 +0300

    partners removed react window

--- src/pages/landingComponents/Partners.tsx
+++ src/pages/landingComponents/Partners.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, memo } from 'react';
-import { Box, Container, Typography, Skeleton, Link } from '@mui/material';
-import { FixedSizeGrid as Grid, GridChildComponentProps } from 'react-window';
+import { Box, Container, Typography, Skeleton, Link, Grid } from '@mui/material';
 import NeuCard from '../../components/neumorphism/card/NeuCard';
 import NeuCardContent from '../../components/neumorphism/card/NeuCardContent';
 import ionosLogo from '../../assets/ionosLogo.jpg';
@@ -33,56 +32,43 @@ const Partners: React.FC = memo(() => {
     return () => clearTimeout(timer);
   }, []);
 
-  const Cell = ({ columnIndex, rowIndex, style }: GridChildComponentProps) => {
-    const index = rowIndex * 4 + columnIndex; // assuming 4 columns
-    if (index >= partners.length) return null;
-    const partner = partners[index];
-
-    return (
-      <Box key={index} style={{ ...style, padding: '16px', boxSizing: 'border-box' }}>
-        {loading ? (
-          <Skeleton variant="rectangular" height={100} sx={{ borderRadius: '25px' }} />
-        ) : (
-          <Link href={partner.link} target="_blank" rel="noopener noreferrer" underline="none">
-            <Box sx={{ boxShadow: 'none', overflow: 'visible' }}>
-              <NeuCard elevation={3} rounded sx={{ padding: '10px'}}>
-                <NeuCardContent>
-                  <Box display="flex" alignItems="center" sx={{ backgroundColor: 'transparent' }}>
-                    <Box 
-                      sx={{
-                        width: 150, 
-                        height: 70, 
-                        backgroundImage: `url(${partner.logo})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center'
-                      }}
-                    />
-                  </Box>
-                </NeuCardContent>
-              </NeuCard>
-            </Box>
-          </Link>
-        )}
-      </Box>
-    );
-  };
-
   return (
-    <Container sx={{ padding: '20px', maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}>
+    <Container sx={{ padding: '20px', maxHeight: 'calc(100vh - 200px)', overflowY: 'auto', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
         <Typography variant="h4">Partners</Typography>
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Grid
-          columnCount={4}
-          columnWidth={225}
-          height={600}
-          rowCount={Math.ceil(partners.length / 4)}
-          rowHeight={150}
-          width={900}
-        >
-          {Cell}
+      <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <Grid container spacing={2} justifyContent="center">
+          {partners.map((partner, index) => (
+            <Grid item xs={12} sm={6} md={3} key={index} sx={{display: 'flex', justifyContent: 'center',}}>
+              <Box sx={{ padding: '16px', boxSizing: 'border-box' }}>
+                {loading ? (
+                  <Skeleton variant="rectangular" height={130} sx={{ borderRadius: '25px' }} />
+                ) : (
+                  <Link href={partner.link} target="_blank" rel="noopener noreferrer" underline="none">
+                    <Box sx={{ boxShadow: 'none', overflow: 'visible' }}>
+                      <NeuCard elevation={3} rounded sx={{ padding: '10px'}}>
+                        <NeuCardContent>
+                          <Box display="flex" alignItems="center" sx={{ backgroundColor: 'transparent' }}>
+                            <Box 
+                              sx={{
+                                width: 150, 
+                                height: 70, 
+                                backgroundImage: `url(${partner.logo})`,
+                                backgroundSize: 'contain',
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: 'center'
+                              }}
+                            />
+                          </Box>
+                        </NeuCardContent>
+                      </NeuCard>
+                    </Box>
+                  </Link>
+                )}
+              </Box>
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </Container>
