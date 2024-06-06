4e5777b11d587d5659b4580610680fc8a2886bc1
Serg <serega101617@gmail.com>
2024-06-06 14:14:29 +0200

    fixed divider in the first panel

--- src/components/AppPageComponents/AppPageCentralComponent.tsx
+++ src/components/AppPageComponents/AppPageCentralComponent.tsx
@@ -1,15 +1,6 @@
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-// import { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
-// import AppPageButtonsComponent from './AppPageButtonsComponent';
-
 
 const AppPageCentralComponent = () => {
-  // const [showOptionsButton, setShowOptionsButton] = useState(false);
-
- 
-
   return (
     <Box
       sx={{
@@ -23,33 +14,6 @@ const AppPageCentralComponent = () => {
       <Box>
         <Typography>Central container</Typography>
       </Box>
-      {/* <Box
-        sx={{
-          margin: '0 auto',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: '20px',
-        }}
-      >
-        <Button
-          variant="outlined"
-          endIcon={
-            showOptionsButton ? (
-              <KeyboardArrowDownIcon />
-            ) : (
-              <KeyboardArrowUpIcon />
-            )
-          }
-          sx={{ width: '300px' }}
-          onClick={() => setShowOptionsButton((prev) => !prev)}
-        >
-          Options
-        </Button>
-        <Collapse in={showOptionsButton}>
-          <AppPageButtonsComponent />
-        </Collapse>
-      </Box> */}
     </Box>
   );
 };
