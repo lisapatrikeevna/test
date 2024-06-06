bbda774310964dddf08dfc8a9d735f410f12b73f
AsligulSafarova <safarovaasligul98@gmail.com>
2024-06-06 09:13:02 +0200

    bug fixed in "ChanelPage"

--- src/pages/Videos/ChannelPage.tsx
+++ src/pages/Videos/ChannelPage.tsx
@@ -8,30 +8,10 @@ import PersonIcon from '@mui/icons-material/Person';
 import NeuButton from "../../components/neumorphism/button/NeuButton";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import banner from "../../assets/banner.jpg";
 
 const ChannelPage: FC = () => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-    // interface Video {
-    //     id: string;
-    //     title: string;
-    //     thumbnail: string;
-    //     views: number;
-    // }
-    // const [videos, setVideos] = useState<Video[]>([]);
-    // useEffect(() => {
-    //     //TODO Выгружать видео конкретно этого пользователя
-    //     const fetchVideos = async () => {
-    //         try {
-    //             const response = await axios.get('/api/videos'); // Substitute your real endpoint.
-    //             setVideos(response.data);
-    //         } catch (error) {
-    //             console.error('Error fetching videos:', error);
-    //         }
-    //     };
-    //
-    //     fetchVideos();
-    // }, []);
 
     const videoId = '';
 
@@ -47,20 +27,25 @@ const ChannelPage: FC = () => {
         <Grid container spacing={2} justifyContent="center" sx={{ maxWidth: '1200px', margin: '0 auto' }}>
             <Grid item xs={12}>
                 <Box component="section">
-                    <img src={avatar} alt="banner" style={{
+                    <img src={banner} alt="banner" style={{
                         width: "100%", height: "210px", objectFit: "cover", padding: 0,
                         margin: 0, borderRadius: "15px"
                     }} />
                 </Box>
                 <Box component="section" sx={{
-                    display: "flex", flexDirection: { xs: "column", sm: "row" },
-                    position: "relative", bottom: { xs: "0px", sm: "-20px" }, left: "0px",
-                    borderBottom: "1px solid grey", alignItems: "center", px: 2
+                    display: "flex",
+                    flexDirection: { xs: "column", sm: "row" },
+                    position: "relative",
+                    bottom: { xs: "0px", sm: "-20px" },
+                    left: "0px",
+                    borderBottom: "1px solid grey",
+                    alignItems: "center",
+                    px: 2
                 }}>
                     <img src={avatar} alt="Avatar" style={{ width: "120px", height: "120px", borderRadius: "50%", objectFit: "cover" }} />
                     <Box component="section" sx={{ ml: { xs: 0, sm: 2 }, mt: { xs: 2, sm: 0 }, textAlign: { xs: 'center', sm: 'left' } }}>
                         <Typography variant="h1" sx={{ fontSize: { xs: "24px", sm: "30px" }, paddingLeft: { xs: "0", sm: "15px" } }}>SHASTUN</Typography>
-                        <CheckCircleIcon sx={{ color: grey[500], fontSize: { xs: "12px", sm: "15px" }, position: "absolute", top: { xs: "5px", sm: "10px" }, left: { xs: "50%", sm: "280px" }, transform: { xs: "translateX(-50%)", sm: "none" } }} />
+                        <CheckCircleIcon sx={{ color: grey[500], fontSize: { xs: "12px", sm: "15px" }, position: "absolute", top: { xs: "8px", sm: "10px" }, left: { xs: "50%", sm: "310px" }, transform: { xs: "translateX(-50%)", sm: "none" } }} />
                         <Box component="section" sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, justifyContent: { xs: "center", sm: "flex-start" }, paddingLeft: "10px", mt: 1 }}>
                             <Typography variant='body1' sx={{ marginLeft: "10px" }}>@Codevolution</Typography>
                             <Typography variant='body1' sx={{ ml: { xs: 0, sm: 2 } }}>634,000 Abonnenten</Typography>
