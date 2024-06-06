958c2017778fe02efbfd01b1440e69b5fe2ad981
AsligulSafarova <safarovaasligul98@gmail.com>
2024-06-06 12:43:02 +0200

    added some  style and deleted  search  from  "VideosMainPage" to "ChannelPage" added skeletons

--- src/pages/Videos/ChannelPage.tsx
+++ src/pages/Videos/ChannelPage.tsx
@@ -29,11 +29,18 @@ const ChannelPage: FC = () => {
         <Grid container spacing={2} justifyContent="center" sx={{ maxWidth: '1200px', margin: '0 auto' }}>
             <Grid item xs={12}>
                 <Box component="section">
-                    {!bannerLoaded && <Skeleton variant="rectangular" width="100%" height={210} sx={{ borderRadius: "15px" }} />}
+                    {!bannerLoaded && <Skeleton variant="rectangular" width="100%" height={210}
+                        sx={{ borderRadius: "15px" }} />}
                     <img
                         src={banner}
                         alt="banner"
-                        style={{ width: "100%", height: "210px", objectFit: "cover", padding: 0, margin: 0, borderRadius: "15px", display: bannerLoaded ? 'block' : 'none' }}
+                        style={{
+                            width: "100%",
+                            height: "210px",
+                            objectFit: "cover",
+                            padding: 0,
+                            margin: 0, borderRadius: "15px", display: bannerLoaded ? 'block' : 'none'
+                        }}
                         onLoad={() => setBannerLoaded(true)}
                     />
                 </Box>
@@ -51,20 +58,44 @@ const ChannelPage: FC = () => {
                     <img
                         src={avatar}
                         alt="Avatar"
-                        style={{ width: "120px", height: "120px", borderRadius: "50%", objectFit: "cover", display: avatarLoaded ? 'block' : 'none' }}
+                        style={{
+                            width: "120px", height: "120px",
+                            borderRadius: "50%", objectFit: "cover",
+                            display: avatarLoaded ? 'block' : 'none'
+                        }}
                         onLoad={() => setAvatarLoaded(true)}
                     />
-                    <Box component="section" sx={{ ml: { xs: 0, sm: 2 }, mt: { xs: 2, sm: 0 }, textAlign: { xs: 'center', sm: 'left' } }}>
-                        <Typography variant="h1" sx={{ fontSize: { xs: "24px", sm: "30px" }, paddingLeft: { xs: "0", sm: "15px" } }}>SHASTUN</Typography>
-                        <CheckCircleIcon sx={{ color: grey[500], fontSize: { xs: "12px", sm: "15px" }, position: "absolute", top: { xs: "8px", sm: "10px" }, left: { xs: "50%", sm: "310px" }, transform: { xs: "translateX(-50%)", sm: "none" } }} />
-                        <Box component="section" sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, justifyContent: { xs: "center", sm: "flex-start" }, paddingLeft: "10px", mt: 1 }}>
+                    <Box component="section" sx={{
+                        ml: { xs: 0, sm: 2 },
+                        mt: { xs: 2, sm: 0 }, textAlign: { xs: 'center', sm: 'left' }
+                    }}>
+                        <Typography variant="h1" sx={{
+                            fontSize: { xs: "24px", sm: "30px" },
+                            paddingLeft: { xs: "0", sm: "15px" }
+                        }}>SHASTUN</Typography>
+                        <CheckCircleIcon sx={{
+                            color: grey[500], fontSize: { xs: "12px", sm: "15px" },
+                            position: "absolute", top: { xs: "8px", sm: "10px" }, left: { xs: "50%", sm: "310px" },
+                            transform: { xs: "translateX(-50%)", sm: "none" }
+                        }} />
+                        <Box component="section" sx={{
+                            display: "flex", flexDirection: { xs: "column", sm: "row" },
+                            justifyContent: { xs: "center", sm: "flex-start" }, paddingLeft: "10px", mt: 1
+                        }}>
                             <Typography variant='body1' sx={{ marginLeft: "10px" }}>@Codevolution</Typography>
                             <Typography variant='body1' sx={{ ml: { xs: 0, sm: 2 } }}>634,000 Abonnenten</Typography>
                             <Typography variant='body1' sx={{ ml: { xs: 0, sm: 2 } }}>1582 Videos</Typography>
                         </Box>
-                        <Typography variant="body1" sx={{ marginLeft: "15px", fontSize: { xs: "14px", sm: "16px" }, mt: 1 }}>Tutorials on the latest tech in web development!</Typography>
-                        <Typography variant="body1" sx={{ marginLeft: "15px", fontSize: { xs: "14px", sm: "16px" }, mt: 1 }}>
-                            <Link href="https://www.example.com" sx={{ color: "blue", textDecoration: "none" }}>https://www.example.com</Link>
+                        <Typography variant="body1" sx={{
+                            marginLeft: "15px", fontSize: { xs: "14px", sm: "16px" },
+                            mt: 1
+                        }}>Tutorials on the latest tech in web development!</Typography>
+                        <Typography variant="body1" sx={{
+                            marginLeft: "15px", fontSize: { xs: "14px", sm: "16px" },
+                            mt: 1
+                        }}>
+                            <Link href="https://www.example.com" sx={{ color: "blue", textDecoration: "none" }}>
+                                https://www.example.com</Link>
                         </Typography>
                         <NeuButton
                             variant="contained"
