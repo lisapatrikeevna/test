8390b6c805cbcbd54dc0bcce2c3e09689dafe81f
Serg <serega101617@gmail.com>
2024-06-17 20:05:21 +0200

    added header in chat

--- src/components/AppPageComponents/AppPageChats.tsx
+++ src/components/AppPageComponents/AppPageChats.tsx
@@ -4,6 +4,9 @@ import {
   TextField,
   InputAdornment,
   IconButton,
+  Typography,
+  Box,
+  Modal,
 } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
@@ -11,6 +14,12 @@ import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt
 import KeyboardVoiceOutlinedIcon from '@mui/icons-material/KeyboardVoiceOutlined';
 import 'react-chat-elements/dist/main.css';
 import { MessageBox } from 'react-chat-elements';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import UserModalProfile from '../../pages/UserModalProfile';
+import { useState } from 'react';
 
 type UserType = {
   id: number;
@@ -23,9 +32,29 @@ type AppPageChatsProps = {
 };
 
 const AppPageChats = ({ currentUser }: AppPageChatsProps) => {
+  const [openUserProfileModal, setOpenUserProfileModal] = useState(false);
+  const [isOpenChatsModal, setIsOpenChatsModal] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+
   if (!currentUser) {
-    return <div>Select a user to start chatting</div>;
+    return <Stack>Select a user to start chatting</Stack>;
   }
+
+  const handleModal = (event: React.MouseEvent<SVGSVGElement>) => {
+    const target = event.currentTarget as unknown as HTMLElement;
+    const iconPosition = target.getBoundingClientRect();
+    const leftOffset = 250;
+    const bottomOffset = 20;
+    setModalPosition({
+      top: iconPosition.bottom + window.scrollY + bottomOffset,
+      left: iconPosition.left + window.scrollX - leftOffset,
+    });
+    setIsOpenChatsModal(true);
+  };
+
+  const handleUserProfileClick = () => {
+    setOpenUserProfileModal(true); // Open userProfile modal
+  };
 
   return (
     <Stack
@@ -37,30 +66,116 @@ const AppPageChats = ({ currentUser }: AppPageChatsProps) => {
       }}
     >
       <Divider sx={{ color: 'black' }} />
-      <Stack width="100%" padding={1} sx={{ flexGrow: 1 }}>
+      <Stack
+        width="100%"
+        padding={1}
+        sx={{ flexGrow: 1, position: 'relative' }}
+      >
         <Stack sx={{ flexGrow: 1 }}>
-          <MessageBox
-            id={currentUser.id}
-            position="left"
-            type="text"
-            title={currentUser.name}
-            text={`This is a chat with ${currentUser.name}`}
-            date={new Date()}
-            focus={false}
-            titleColor="#000"
-            forwarded={false}
-            replyButton={false}
-            removeButton={false}
-            retracted={false}
-            status="sent"
-            notch={false}
-          />
+          <Stack
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              bgcolor: 'aqua',
+              position: 'absolute',
+              width: '100%',
+              zIndex: '100',
+              padding: '10px',
+              top: 0,
+              left: 0,
+              right: 0,
+            }}
+          >
+            <Stack
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: '10px',
+              }}
+            >
+              <ArrowBackIcon cursor="pointer" />
+              <Typography
+                sx={{ cursor: 'pointer' }}
+                onClick={handleUserProfileClick}
+              >
+                {currentUser.name}
+              </Typography>
+            </Stack>
+            <Stack
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: '10px',
+              }}
+            >
+              <SearchOutlinedIcon cursor="pointer" />
+              <PhoneIcon cursor="pointer" />
+              <MoreVertIcon cursor="pointer" onClick={handleModal} />
+            </Stack>
+            <UserModalProfile
+              open={openUserProfileModal}
+              onClose={() => setOpenUserProfileModal(false)}
+              currentName={currentUser.name}
+            />
+            <Modal
+              open={isOpenChatsModal}
+              onClose={() => setIsOpenChatsModal(false)}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              // BackdropProps={{ style: { backgroundColor: 'transparent' } }}
+            >
+              <Box
+                sx={{
+                  padding: '15px',
+                  borderRadius: '15px',
+                  width: '300px',
+                  background: '#e0e0e0',
+                  position: 'absolute',
+                  top: modalPosition.top,
+                  left: modalPosition.left,
+                }}
+              >
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Duis mollis, est non commodo luctus, nisi erat porttitor
+                  ligula.
+                </Typography>
+              </Box>
+            </Modal>
+          </Stack>
+          <Stack sx={{ marginTop: '40px' }}>
+            <MessageBox
+              id={currentUser.id}
+              position="left"
+              type="text"
+              title={currentUser.name}
+              text={`This is a chat with ${currentUser.name}`}
+              date={new Date()}
+              focus={false}
+              titleColor="#000"
+              forwarded={false}
+              replyButton={true}
+              removeButton={false}
+              retracted={false}
+              status="sent"
+              notch={false}
+            />
+          </Stack>
         </Stack>
 
         <Stack direction="row" alignItems="center">
           <TextField
             size="small"
             fullWidth
+            autoComplete="off"
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
